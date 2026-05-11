5c9b2e4eb681c44087fa908a7b1768d92b6ce8f3
AxelM <alex.starkpc@outlook.fr>
2026-05-11 17:39:14 +0200

    Data filtering

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # supervised-sorting-model
 
+1. Visualisation par histogramme : peu compréhensible
+2. Visualition par plot : plus lisible pour notre usecase
+
+3. Essai de tri par quartiles : pas assez de précision
+4. Essai de tri en définissant une valeur maximale manuellement (ex speeding_violation) : OK
+
--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
+#  1   age                  10000 non-null  int64  
+#  2   gender               10000 non-null  int64  
+#  3   driving_experience   10000 non-null  object 
+#  4   education            10000 non-null  object 
+#  5   income               10000 non-null  object 
+#  6   credit_score         9018 non-null   float64
+#  7   vehicle_ownership    10000 non-null  float64
+#  8   vehicle_year         10000 non-null  object 
+#  9   married              10000 non-null  float64
+#  10  children             10000 non-null  float64
+#  11  postal_code          10000 non-null  int64  
+#  12  annual_mileage       9043 non-null   float64
+#  13  vehicle_type         10000 non-null  object 
+#  14  speeding_violations  10000 non-null  int64  
+#  15  duis                 10000 non-null  int64  
+#  16  past_accidents       10000 non-null  int64  
+#  17  outcome              10000 non-null  float64
 
 def main():
     # Lire le fichier CSV dans un DataFrame pandas.
@@ -16,6 +35,35 @@ def main():
         # Detection des valeurs manquantes
         print("\n\nNombre de valeurs manquantes par colonne :")
         print(df.isnull().sum())
+        
+
+
+
+        print("\n\nGénération des histogrammes...")
+
+        # retirer speeding violation si la valeur depasse 500
+        numeric_cols = ['speeding_violations']    
+        for col in numeric_cols:
+            df = df[df[col] <= 100]
+
+
+        # Génération des multiples boîtes à moustaches
+        df.plot(
+            kind='box',         
+            subplots=True,      
+            layout=(-1, 4),        
+            figsize=(18, 12),     
+            sharex=False,         
+            sharey=False         
+        )
+
+        plt.tight_layout()
+        plt.show()
+
+    
+    
+        
+
     except FileNotFoundError:
         print("Erreur : Fichier non trouvé")
 
